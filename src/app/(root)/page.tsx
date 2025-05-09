96f2440ad78b19c61196f0cc12df5f77f56004d0
louieBuera <isaac.277.elifer@gmail.com>
2025-05-09 13:35:16 +0800

    Remove shadcn component + dependencies

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -15,7 +15,7 @@ async function Home() {
       </section>
       {/* <h1 className="text-2xl font-semibold">{ process.env.MONGODB_URI }</h1> */}
 
-      <h1 className="text-2xl font-semibold">Pet Count: { pets.length }</h1>
+      {/* <h1 className="text-2xl font-semibold">Pet Count: { pets.length }</h1> */}
       <main className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-4'>
         <div className='grid gap-4 mt-4 ml-4 mb-4'>
           <PawCard details={images.image1}/>
